97a28e1fd7a7790ab8aed2a1cfb4650d64ed3cdb
Liu Chang <lllccc2009@gmail.com>
2015-08-09 20:53:41 -0400

    add ex1~6

---
+++ learn_py_the_hard_way/ex2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+## A coment, this is so you can read your program letter
+# Anything after the # is ignored by python
+print "I could have code like this." # and the comment after is ignored
+
+# you can also use a comment to "disable" or comment out some code
+# print "This won't run"
+print "This will run"
